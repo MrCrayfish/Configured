74bb2a2b0d5e069600c9e7767fd1f681496992e5
MrCrayfish <mrcrayfish@hotmail.com>
2021-04-28 16:49:02 +0930

    Fixed server incompatibility issue when mod installed only on client

--- src/main/java/com/mrcrayfish/configured/Configured.java
+++ src/main/java/com/mrcrayfish/configured/Configured.java
@@ -3,12 +3,15 @@ package com.mrcrayfish.configured;
 import com.mrcrayfish.configured.client.ClientHandler;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLLoader;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -26,8 +29,8 @@ public class Configured
         {
             ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.clientSpec);
         }
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        bus.addListener(this::onConstructEvent);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onConstructEvent);
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 
     private void onConstructEvent(FMLLoadCompleteEvent event)
