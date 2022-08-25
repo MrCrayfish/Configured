0701c87a793f9610f0ad154585bd9c50a4030d94
MrCrayfish <mrcrayfish@hotmail.com>
2022-08-26 01:19:01 +0930

    🐛 Fixed "null" being acceptable value for enum properties

--- src/main/java/com/mrcrayfish/configured/api/simple/EnumProperty.java
+++ src/main/java/com/mrcrayfish/configured/api/simple/EnumProperty.java
@@ -4,25 +4,37 @@ import com.electronwill.nightconfig.core.ConfigSpec;
 import com.electronwill.nightconfig.core.EnumGetMethod;
 import com.google.common.base.Preconditions;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 /**
  * Author: MrCrayfish
  */
 public final class EnumProperty<T extends Enum<T>> extends ConfigProperty<T>
 {
-    EnumProperty(T defaultValue)
+    private final EnumGetMethod method;
+
+    EnumProperty(T defaultValue, EnumGetMethod method)
     {
         super(defaultValue, (config, path) -> config.getEnumOrElse(path, defaultValue));
+        this.method = method;
     }
 
     @Override
     public void defineSpec(ConfigSpec spec)
     {
         Preconditions.checkState(this.data != null, "Config property is not initialized yet");
-        spec.defineEnum(this.data.getPath(), this.defaultValue, EnumGetMethod.NAME_IGNORECASE);
+        Collection<T> values = Arrays.asList(this.defaultValue.getDeclaringClass().getEnumConstants());
+        spec.defineRestrictedEnum(this.data.getPath(), this.defaultValue, values, this.method);
     }
 
     public static <T extends Enum<T>> EnumProperty<T> create(T defaultValue)
     {
-        return new EnumProperty<>(defaultValue);
+        return new EnumProperty<>(defaultValue, EnumGetMethod.NAME_IGNORECASE);
+    }
+
+    public static <T extends Enum<T>> EnumProperty<T> create(T defaultValue, EnumGetMethod method)
+    {
+        return new EnumProperty<>(defaultValue, method);
     }
 }
