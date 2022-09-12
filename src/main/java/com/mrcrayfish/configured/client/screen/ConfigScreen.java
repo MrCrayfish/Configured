b045e87f0c6a7ceda2751a54a053d8b8430023d8
MrCrayfish <mrcrayfish@hotmail.com>
2022-09-13 02:46:40 +0930

    📦 Made read-only tooltip consistent

--- src/main/java/com/mrcrayfish/configured/client/screen/ConfigScreen.java
+++ src/main/java/com/mrcrayfish/configured/client/screen/ConfigScreen.java
@@ -296,7 +296,7 @@ public class ConfigScreen extends ListMenuScreen implements IEditing
             Screen.blit(poseStack, this.width - 30, 14, 20, 20, 0, 33, 10, 10, 64, 64);
             if(ScreenUtil.isMouseWithin(this.width - 30, 14, 20, 20, mouseX, mouseY))
             {
-                this.setActiveTooltip(new TranslatableComponent("configured.gui.read_only_config").withStyle(ChatFormatting.BLUE));
+                this.setActiveTooltip(new TranslatableComponent("configured.gui.read_only_config"), 0xFF1E6566);
             }
         }
 
