c0bcdb2fe2d20cfd4a27d241c0623df006b22048
MrCrayfish <mrcrayfish@hotmail.com>
2022-09-23 02:43:06 +0930

    🐛 Fixed icons and labels to match config

--- src/main/java/com/mrcrayfish/configured/client/screen/WorldSelectionScreen.java
+++ src/main/java/com/mrcrayfish/configured/client/screen/WorldSelectionScreen.java
@@ -138,10 +138,27 @@ public class WorldSelectionScreen extends ListMenuScreen
                 this.iconFile = null;
             }
             this.texture = this.loadWorldIcon();
-            //TODO fix icon for and label for read-only
-            this.modifyButton = new IconButton(0, 0, 0, 22, 60, Component.translatable("configured.gui.select"), onPress -> {
+            this.modifyButton = new IconButton(0, 0, 0, this.getIconV(), 60, this.getButtonLabel(), onPress -> {
                 this.loadWorldConfig(summary.getLevelId(), summary.getLevelName());
             });
+        }
+
+        private Component getButtonLabel()
+        {
+            if(WorldSelectionScreen.this.config.isReadOnly())
+            {
+                return Component.translatable("configured.gui.view");
+            }
+            return Component.translatable("configured.gui.select");
+        }
+
+        private int getIconV()
+        {
+            if(WorldSelectionScreen.this.config.isReadOnly())
+            {
+                return 33;
+            }
+            return 22;
         }
 
         @Override
